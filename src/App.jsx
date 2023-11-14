6c8f6b7f5134c45f094759cbcb0f4d8499421df0
akcode2 <40044933+akcode2@users.noreply.github.com>
2023-11-14 10:54:20 -0600

    Complete code and styling for 9 card game

--- src/App.jsx
+++ src/App.jsx
@@ -3,52 +3,111 @@ import "./App.css";
 
 const baseURL = "https://pokeapi.co/api/v2/pokemon";
 
-function Card({ pokedexNmbr, onCardClick}) {
-  const [name, setName] = useState('');
-  const [source, setSource] = useState('');
+function Card({ pokedexNmbr, onCardClick }) {
+  const [name, setName] = useState("");
+  const [source, setSource] = useState("");
 
   useEffect(() => {
     fetch(`${baseURL}/${pokedexNmbr}`)
-    .then(response => response.json())
-    .then(data => {
-      setName(data.name.charAt(0).toUpperCase() + data.name.slice(1));
-      setSource(data.sprites.front_default)
-    })
-    .catch(error => console.error("Error: ", error))
-  }, [pokedexNmbr])
+      .then((response) => response.json())
+      .then((data) => {
+        setName(data.name.charAt(0).toUpperCase() + data.name.slice(1));
+        setSource(data.sprites.front_default);
+      })
+      .catch((error) => console.error("Error: ", error));
+  }, []);
 
   return (
-    <button onClick={() => {onCardClick(pokedexNmbr)}}>
-      <img src={source} alt={`Pokemon# ${pokedexNmbr}`}/>
+    <button
+      className="card"
+      onClick={() => {
+        onCardClick(pokedexNmbr);
+      }}
+    >
+      <img className="sprite" src={source} alt={`Pokemon# ${pokedexNmbr}`} />
       <h4>{name}</h4>
     </button>
-  )
+  );
 }
 
 function App() {
-  const [round, setRound] = useState(0);
-  const [cardsToShow, setCardsToShow] = useState([]);
-  const [chosen, setChosen] = useState([]);
+  // const [history, setHistory] = useState([Array(5).fill(null)]);
+  const [round, setRound] = useState(1);
+  const [status, setStatus] = useState("New game");
+  const [choices, setChoices] = useState([]);
+
+  const handleStart = () => {
+    setStatus("In progress");
+  };
 
   const handleChoice = (choice) => {
-    const nextChosen = chosen.slice();
-    nextChosen.push(choice)
-    setChosen(nextChosen);
+    if (choices.includes(choice)) {
+      setStatus("You lose");
+    } else if (round === 8) {
+      setStatus("You win");
+    } else {
+      const nextChoices = choices.slice();
+      nextChoices.push(choice);
+      setChoices(nextChoices);
+      setRound((prevRound) => prevRound + 1);
+    }
+  };
+
+  const randomChoices = [];
+
+  for (let i = 0; i < 9 - (round - 1); i++) {
+    let randomPokemon = Math.floor(Math.random() * 252);
+    // Try again if the random pokemon is in the choices
+    while (
+      choices.includes(randomPokemon || randomChoices.includes(randomPokemon))
+    ) {
+      randomPokemon = Math.floor(Math.random() * 252);
+    }
+    randomChoices.push(randomPokemon);
   }
 
-  let pokedexNmbr = Math.floor(Math.random() * 252);
+  const cardsToShow = [...choices, ...randomChoices];
+  const shuffledCards = shuffleArray(cardsToShow);
 
   return (
     <>
-      <div className="header"></div>
       <div className="game">
-        <div className="cardContainer">
-          <Card pokedexNmbr={pokedexNmbr} onCardClick={handleChoice}/>
-        </div>
+        {status === "New game" && (
+          <button onClick={handleStart} className="startGame">
+            Start game
+          </button>
+        )}
+
+        {status === "In progress" && (
+          <div className="cardContainer">
+            {shuffledCards.map((pokedexNmbr, index) => {
+              return (
+                <Card
+                  key={`${pokedexNmbr} ${index}`}
+                  pokedexNmbr={pokedexNmbr}
+                  onCardClick={handleChoice}
+                />
+              );
+            })}
+          </div>
+        )}
+        {status === "You win" && (
+          <p>You win! Game over.</p>
+        )}
+        {status === "You lose" && (
+          <p>You lose! Game over.</p>
+        )}
       </div>
-      <div className="footer"></div>
     </>
   );
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 export default App;
